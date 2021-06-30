fa8cf4ca436f886e956c1ae0196049c932f876dd
vlada-s <vlada.shyshkovska@gmail.com>
2021-06-30 16:17:47 +0300

    Program was created

---
+++ addressbook-web-test2/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace addressbook_web_test2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
